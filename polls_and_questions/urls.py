1f5a257830b9b79f5b93bd6f4a83a54325ee31d9
Erisel Almarales Vázquez <erisel.almarales@gmail.com>
2022-07-13 08:16:20 -0400

    adding create qanswers feature

--- polls_and_questions/urls.py
+++ polls_and_questions/urls.py
@@ -4,12 +4,20 @@ from django.urls import path
 
 from polls_and_questions import views
 
+
 urlpatterns = [
 
     path('watchit/<uuid:watchit_uuid>/poll/configuration/', views.DefaultConfigPollManagerApiView.as_view()),
     path('watchit/<uuid:watchit_uuid>/qa/configuration/', views.DefaultConfigQuestionManagerApiView.as_view()),
 
+    path('watchit/<uuid:watchit_uuid>/question/', views.QuestionCreatorManager.as_view()),
+
+
     # path('watchit/<uuid:watchit_uuid>/poll/<int:poll_id>/', views.PollManagerApiView.as_view()),
     path('watchit/<uuid:watchit_uuid>/question/<int:question_id>/', views.QuestionManagerApiView.as_view()),
+
+    path('watchit/<uuid:watchit_uuid>/question/<int:question_id>/answers/<int:answer_id>/', views.QAnswerManagerApiView.as_view()),
+
+    path('watchit/<uuid:watchit_uuid>/question/<int:question_id>/answers/', views.QAnswerCreatorManager.as_view()),
 
 ]
